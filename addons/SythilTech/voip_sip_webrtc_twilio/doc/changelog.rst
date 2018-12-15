232b6df39fb37d2614de17b0e8758a5855639688
nguyenductamlhp <nguyenductamlhp@gmail.com>
2018-12-15 15:40:54 +0700

    [RM] Remove addon

--- addons/SythilTech/voip_sip_webrtc_twilio/doc/changelog.rst
+++
@@ -1,7 +0,0 @@
-v1.0.4
-======
-* Merge with twilio bill and introduce manual call functionality
-
-v1.0.0
-======
-* Port to version 11
--- addons/SythilTech/voip_sip_webrtc_twilio/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import voip_twilio
-from . import voip_call
-from . import voip_call_wizard
-from . import res_partner
-from . import voip_number
