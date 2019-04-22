469ee2d196da0a0ddc69f9ec19f6ad5682048be2
Nguyen Duc Tam <nguyenductamlhp@gmail.com>
2019-04-22 22:57:18 +0700

    [ADD] Add module

---
+++ project/zaloo/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Zalo Chat",
+    'version': '11.0.1.0.0',
+    "author": "PL Laboratory",
+    "website": "http://pllabvn.com",
+    'license': 'AGPL-3',
+    'category': '',
+    "depends": [
+        "base",
+        "contacts",
+    ],
+    'data': [
+        'view/base/res_partner_view.xml',
+    ],
+    'installable': True,
+}
