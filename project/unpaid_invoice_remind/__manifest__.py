5500fadadb809e6601013fa9c86fcf6d9741665e
nguyenductamlhp <nguyenductamlhp@gmail.com>
2019-11-28 21:37:22 +0700

    [ADD] Scheduler remind

--- project/unpaid_invoice_remind/__manifest__.py
+++ project/unpaid_invoice_remind/__manifest__.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "unpaid_invoice_remind",
+    'name': "Unpaid Invoice Remind",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Send email to remind late paiding invoice""",
 
     'description': """
-        Long description of module's purpose
+        Send email to remind late paiding invoice
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "nguyenductamlhp",
+    'website': "http://www.pllabvn.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '11.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['account_invoicing'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'data/mail_template_data.xml',
+        'data/ir_cron_data.xml',
     ],
 }
