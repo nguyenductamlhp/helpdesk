76a65f31ad057da5a60d4733d200c3050afc0301
tamnd <tamnd@gmail.com>
2019-12-02 08:27:28 +0700

    [ADD] Create task from ticket

--- helpdesk_project/models/helpdesk/helpdesk_ticket.py
+++ helpdesk_project/models/helpdesk/helpdesk_ticket.py
@@ -7,6 +7,22 @@ class HelpdeskTicket(models.Model):
 
     task_ids = fields.One2many(
         'project.task', 'helpdesk_ticket_id',
-        string="Tasks")
+        string="Tasks", track_visibility='onchange')
     project_id = fields.Many2one(
-        'project.project', string='Project')
+        'project.project', string='Project', track_visibility='onchange')
+
+    @api.multi
+    def create_project_task(self):
+        """
+        Create new project task and link to helpdesk ticket
+        """
+        Task = self.env['project.task']
+        for ticket in self:
+            Task.create({
+                'project_id': ticket.project_id and ticket.project_id.id,
+                'user_id': ticket.user_id and ticket.user_id.id,
+                'helpdesk_ticket_id': ticket.id,
+                'name': ticket.name,
+                'description': ticket.description,
+                'partner_id': ticket.partner_id and ticket.partner_id.id
+            })
