2729fb35a0c2843519e4a7bd55e2e4f2c71ef454
tamnd <tamnd@gmail.com>
2019-12-20 13:44:26 +0700

    [RM] Remove helpdesk

--- addons/helpdesk/helpdesk_project/models/project/project_task.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Project_task(models.Model):
-    _inherit = 'project.task'
-
-    helpdesk_ticket_id = fields.Many2one(
-        'helpdesk.ticket', help='Link to helpdesk ticket if exist')
