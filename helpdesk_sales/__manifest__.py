5c0740ea26983e4779a7f5dbc7d8534e0a47121f
tamnd <tamnd@gmail.com>
2019-12-20 13:52:42 +0700

    [MOD] Content for module

--- helpdesk_sales/__manifest__.py
+++ helpdesk_sales/__manifest__.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "helpdesk_sales",
-
+    'name': "Helpdesk Sales",
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+Sale support service on helpdesk ticket
+""",
     'description': """
-        Long description of module's purpose
+Features:
+- Add estimation by hour on helpdesk ticket
+- Create Sale order from helpdesk ticket
     """,
+    'author': "nguyenductamlhp",
+    'website': "http://www.pllabvn.com",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Helpdesk',
+    'version': '11.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['helpdesk_mgnt', 'sale_management'],
 
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
