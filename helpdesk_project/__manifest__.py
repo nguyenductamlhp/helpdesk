63b186b39d6fb5d8adb97044edb7eabc3c37873e
nguyenductamlhp <nguyenductamlhp@gmail.com>
2019-12-22 23:32:32 +0700

    [RE] Restore code helpdesk project modulw

--- helpdesk_project/__manifest__.py
+++ helpdesk_project/__manifest__.py
@@ -3,33 +3,29 @@
     'name': "helpdesk_project",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Link between project and helpdesk module""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "nguyenductamlhp",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '11.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['project', 'helpdesk_mgmt'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+
+        'views/helpdesk/helpdesk_ticket_view.xml',
+        'views/project/project_task_view.xml',
     ],
 }
